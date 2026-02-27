844477f6e241ce6e19af8ef8241a4df3b3334af0
anowakowski <nowakowski.tools@gmail.com>
2026-02-27 15:21:59 +0100

    init project

---
+++ GloboTicket.Admin/GloboTicket.Admin.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GloboTicket.Admin.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GloboTicket.Admin/GloboTicket.Admin.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GloboTicket.Admin.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
